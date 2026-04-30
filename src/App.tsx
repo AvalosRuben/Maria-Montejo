998e612327a7cf01d26550fa4007c437b86d181f
AvalosRuben <rubenavalosmtz@gmail.com>
2026-04-30 13:09:50 -0600

    hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CoffeeShop from "./components/CoffeeShop";
 import TopBar from "./components/TopBar";
 import "./index.css";
 
@@ -5,14 +6,7 @@ function App() {
   return (
     <>
       <TopBar />
-      <div className="w-screen h-screen bg-accent flex flex-col justify-center items-center">
-        <div className="flex flex-col gap-6 items-center">
-          <h1 className="text-9xl text-foreground font-serif">
-            Maria & Montejo
-          </h1>
-          <p className="text-4xl text-foreground font-serif">Próximamente...</p>
-        </div>
-      </div>
+      <CoffeeShop />
     </>
   );
 }
---
+++ src/components/CoffeeShop.tsx
@@ -0,0 +1,11 @@
+function CoffeeShop() {
+  return (
+    <div className="bg-[url(/mariaymontejohero.jpg)] w-screen h-screen bg-accent flex flex-col justify-center items-center">
+      <div className="flex flex-col gap-6 items-center">
+        <h1 className="text-9xl font-serif text-background">Maria & Montejo</h1>
+        <p className="text-4xl text-background font-serif">Próximamente...</p>
+      </div>
+    </div>
+  );
+}
+export default CoffeeShop;
