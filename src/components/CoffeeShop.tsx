6b17c1bd1100a579c217cc826df653b1dfe36833
AvalosRuben <rubenavalosmtz@gmail.com>
2026-06-24 18:46:06 -0600

    feat(responsiveness): Made the text and photo section responsive

--- src/components/CoffeeShop.tsx
+++ src/components/CoffeeShop.tsx
@@ -1,7 +1,7 @@
 function CoffeeShop() {
   return (
     <>
-      <div className="bg-[url(/mariaymontejohero.jpg)] bg-cover bg-center w-full min-h-screen bg-accent flex flex-col justify-center items-center">
+      <div className="bg-[url(/mariaymontejohero.jpg)] bg-cover bg-center w-full min-h-screen bg-accent flex flex-col justify-center items-center mb-15 ">
         <div className="flex flex-col gap-6 items-center">
           <h1 className="text-5xl sm:text-7xl md:text-9xl font-serif text-background">
             Maria & Montejo
@@ -11,25 +11,26 @@ function CoffeeShop() {
           </p>
         </div>
       </div>
-      <div className="w-screen h-screen flex items-center justify-center gap-50 border-b border-border">
-        <div className="w-3xl h-full flex flex-col items-center justify-center gap-8 text-wrap">
-          <h2 className="font-serif text-6xl text-left">
+      <div className="w-full min-h-screen flex flex-col lg:flex-row  items-center justify-center gap-12 p-6 md:p-12 max-w-7xl mx-auto border-b border-border ">
+        <div className="w-full lg:max-w-3xl flex flex-col items-center justify-center gap-6 md:gap-8">
+          <h2 className="font-serif text-3xl sm:text-5xl md:text-7xl text-center md:text-left w-full">
             Un espacio donde el tiempo se detiene
           </h2>
-          <p className="text-left text-xl font-extralight text-muted-foreground">
+          <p className="text-center lg:text-left text-lg md:text-xl font-extralight text-muted-foreground">
             En el corazón de Mérida, María & Montejo nace como un refugio de
             calma y autenticidad. Nuestro espacio combina la arquitectura
             tradicional yucateca con una estética contemporánea y minimalista.
           </p>
-          <p className="text-left text-xl font-extralight text-muted-foreground">
+          <p className="text-center lg:text-left text-lg md:text-xl font-extralight text-muted-foreground">
             Cada taza es una invitación a desacelerar, a conectar con el momento
             presente y a disfrutar de un café preparado con dedicación.
           </p>
         </div>
-        <div className="w-xl">
+        <div className="w-full lg:max-w-xl flex justify-center items-center">
           <img
             src="src/assets/mariaymontejointerior.png"
             alt="Interior Maria y Montejo"
+            className="w-full h-auto max-h-[500px] object-cover"
           />
         </div>
       </div>
